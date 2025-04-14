4b869c21e7dd03470c16bb555b9ebb247fd6d6dc
vudinhso <vudinhso@gmail.com>
2025-04-14 23:14:06 +0700

    change package name

--- build/my_robot_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vudinhso/RMIT_Bot/lesson3_ws/src/my_robot_controller
-# Build directory: /home/vudinhso/RMIT_Bot/lesson3_ws/build/my_robot_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
